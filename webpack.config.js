830d6ca97b3e09b89c24b14d60e8322e1e1906fa
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2025-01-06 21:49:26 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 const path = require('path');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+
   entry: './worker.js',
   output: {
     filename: 'bundle.js',
