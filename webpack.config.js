2938dbb588158303c1372f55083443a2d898e682
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2025-01-06 21:09:46 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './worker.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'webworker',
+  mode: 'production',
+};
